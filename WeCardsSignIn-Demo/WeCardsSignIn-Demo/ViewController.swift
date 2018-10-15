15a786a0b1cbca101338ee6fdb1061a88ccdace9
Nirav <nirav@ccs.sg>
2018-10-15 12:48:53 +0530

    Initial Setup

---
+++ WeCardsSignIn-Demo/WeCardsSignIn-Demo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  WeCardsSignIn-Demo
+//
+//  Created by Nirav Patel on 04-10-18.
+//  Copyright © 2018 Nirav Patel. All rights reserved.
+//
+
+import UIKit
+import WeCardsSignIn
+import Alamofire
+
+class ViewController: UIViewController, WeCardsSignInDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        WCSignIn.sharedInstance().delegate = self
+        WCSignIn.sharedInstance().presentViewController(viewController: self)
+    }
+
+    //MARK:- User Defined Delegate Methods
+    
+    func onAuthenticationSuccessful(dictionary: NSDictionary) {
+        print("\(dictionary)")
+    }
+    
+    func onAuthenticationFail(message: String) {
+        print("\(message)")
+    }
+    
+    func onSignOutSuccessful(dictionary: NSDictionary) {
+        print("\(dictionary)")
+    }
+}
+
